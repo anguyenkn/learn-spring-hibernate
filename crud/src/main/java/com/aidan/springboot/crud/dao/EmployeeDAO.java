028e9279df7f66d0af0e7d70d94e1dec5efa9acb
Aidan Nguyen <anguyenkn@gmail.com>
2020-08-23 10:04:30 -0700

    crud stuffs

---
+++ crud/src/main/java/com/aidan/springboot/crud/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.aidan.springboot.crud.dao;
+
+import java.util.List;
+
+import com.aidan.springboot.crud.entity.Employee;
+
+public interface EmployeeDAO {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
---
+++ crud/src/test/java/com/aidan/springboot/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidan.springboot.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
